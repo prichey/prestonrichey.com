b8f0ead82b7da2d5d21a8351d15a24a6eecfc64e
Preston Richey <prichey@barkleyus.com>
2017-11-30 10:50:30 -0600

    Tweak RT copy; Add gatsby-transformer-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
