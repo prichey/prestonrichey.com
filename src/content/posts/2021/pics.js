4d68d71837786e6525b1c8144c32301cc155faa9
Preston Richey <prestonleerichey@gmail.com>
2022-01-09 23:04:22 -0500

    Very WIP

---
+++ src/content/posts/2021/pics.js
@@ -0,0 +1,79 @@
+import bikeSelf from './bike-self.jpg';
+import bikeSelfZoom from './bike-self-zoom.jpg';
+import doorFlowers from './door-flowers.jpg';
+import doorFlowersZoom from './door-flowers-zoom.jpg';
+import canyonRiverview from './canyon-riverview.jpg';
+import canyonRiverviewZoom from './canyon-riverview-zoom.jpg';
+import canyonSunrise from './canyon-sunrise.jpg';
+import canyonSunriseZoom from './canyon-sunrise-zoom.jpg';
+import leafy from './leafy.jpg';
+import leafyZoom from './leafy-zoom.jpg';
+import jamesLamp from './james-lamp.jpg';
+import jamesLampZoom from './james-lamp-zoom.jpg';
+import davidLamp from './david-lamp.jpg';
+import davidLampZoom from './david-lamp-zoom.jpg';
+import haCouple from './ha-couple.jpg';
+import haCoupleZoom from './ha-couple-zoom.jpg';
+import chanterelles from './chanterelles.jpg';
+import chanterellesZoom from './chanterelles-zoom.jpg';
+import candleSteps from './candle-steps.jpg';
+import candleStepsZoom from './candle-steps-zoom.jpg';
+import blueSun from './blue-sun.jpg';
+import blueSunZoom from './blue-sun-zoom.jpg';
+import hudson from './hudson.jpg';
+import hudsonZoom from './hudson-zoom.jpg';
+import bikeCamp from './bike-camp.jpg';
+import bikeCampZoom from './bike-camp-zoom.jpg';
+import frybread from './frybread.jpg';
+import frybreadZoom from './frybread-zoom.jpg';
+import hollywood from './hollywood.jpg';
+import hollywoodZoom from './hollywood-zoom.jpg';
+import texasSunset from './texas-sunset.jpg';
+import texasSunsetZoom from './texas-sunset-zoom.jpg';
+import roller from './roller.jpg';
+import rollerZoom from './roller-zoom.jpg';
+import hollyCam from './holly-cam.jpg';
+import hollyCamZoom from './holly-cam-zoom.jpg';
+import parentsSelfie from './parents-selfie.jpg';
+import parentsSelfieZoom from './parents-selfie-zoom.jpg';
+
+export {
+  bikeSelf,
+  bikeSelfZoom,
+  doorFlowers,
+  doorFlowersZoom,
+  canyonRiverview,
+  canyonRiverviewZoom,
+  canyonSunrise,
+  canyonSunriseZoom,
+  leafy,
+  leafyZoom,
+  jamesLamp,
+  jamesLampZoom,
+  davidLamp,
+  davidLampZoom,
+  haCouple,
+  haCoupleZoom,
+  chanterelles,
+  chanterellesZoom,
+  candleSteps,
+  candleStepsZoom,
+  blueSun,
+  blueSunZoom,
+  hudson,
+  hudsonZoom,
+  bikeCamp,
+  bikeCampZoom,
+  frybread,
+  frybreadZoom,
+  hollywood,
+  hollywoodZoom,
+  texasSunset,
+  texasSunsetZoom,
+  roller,
+  rollerZoom,
+  hollyCam,
+  hollyCamZoom,
+  parentsSelfie,
+  parentsSelfieZoom
+};
