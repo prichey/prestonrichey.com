424ff4441a78aea611fed8507fa9f3d283533411
Preston Richey <prestonleerichey@gmail.com>
2022-01-17 12:33:23 -0500

    Add actual pictures

--- src/content/posts/2021/pics.js
+++ src/content/posts/2021/pics.js
@@ -1,51 +1,107 @@
-import bikeSelf from './bike-self.jpg';
-import bikeSelfZoom from './bike-self-zoom.jpg';
-import doorFlowers from './door-flowers.jpg';
-import doorFlowersZoom from './door-flowers-zoom.jpg';
-import canyonRiverview from './canyon-riverview.jpg';
-import canyonRiverviewZoom from './canyon-riverview-zoom.jpg';
-import canyonSunrise from './canyon-sunrise.jpg';
-import canyonSunriseZoom from './canyon-sunrise-zoom.jpg';
-import leafy from './leafy.jpg';
-import leafyZoom from './leafy-zoom.jpg';
-import jamesLamp from './james-lamp.jpg';
-import jamesLampZoom from './james-lamp-zoom.jpg';
-import davidLamp from './david-lamp.jpg';
-import davidLampZoom from './david-lamp-zoom.jpg';
-import haCouple from './ha-couple.jpg';
-import haCoupleZoom from './ha-couple-zoom.jpg';
-import chanterelles from './chanterelles.jpg';
-import chanterellesZoom from './chanterelles-zoom.jpg';
-import candleSteps from './candle-steps.jpg';
-import candleStepsZoom from './candle-steps-zoom.jpg';
-import blueSun from './blue-sun.jpg';
-import blueSunZoom from './blue-sun-zoom.jpg';
-import hudson from './hudson.jpg';
-import hudsonZoom from './hudson-zoom.jpg';
-import bikeCamp from './bike-camp.jpg';
-import bikeCampZoom from './bike-camp-zoom.jpg';
-import frybread from './frybread.jpg';
-import frybreadZoom from './frybread-zoom.jpg';
-import hollywood from './hollywood.jpg';
-import hollywoodZoom from './hollywood-zoom.jpg';
-import texasSunset from './texas-sunset.jpg';
-import texasSunsetZoom from './texas-sunset-zoom.jpg';
-import roller from './roller.jpg';
-import rollerZoom from './roller-zoom.jpg';
-import hollyCam from './holly-cam.jpg';
-import hollyCamZoom from './holly-cam-zoom.jpg';
-import parentsSelfie from './parents-selfie.jpg';
-import parentsSelfieZoom from './parents-selfie-zoom.jpg';
+// az
+import brittanCanyon from './az/brittan-canyon.jpg';
+import brittanCanyonZoom from './az/brittan-canyon-zoom.jpg';
+import canyonRiverview from './az/canyon-riverview.jpg';
+import canyonRiverviewZoom from './az/canyon-riverview-zoom.jpg';
+import canyonSunrise from './az/canyon-sunrise.jpg';
+import canyonSunriseZoom from './az/canyon-sunrise-zoom.jpg';
+import frybread from './az/frybread.jpg';
+import frybreadZoom from './az/frybread-zoom.jpg';
 
-const proud = [
-  { src: bikeSelf, zoomSrc: bikeSelfZoom },
-  { src: doorFlowers, zoomSrc: doorFlowersZoom },
-  { src: blueSun, zoomSrc: blueSunZoom, caption: 'FPO' }
-];
+// co
+import beaverMountain from './co/beaver-mountain.jpg';
+import beaverMountainZoom from './co/beaver-mountain-zoom.jpg';
+import chanterelles from './co/chanterelles.jpg';
+import chanterellesZoom from './co/chanterelles-zoom.jpg';
+import colleenRowan from './co/colleen-rowan.jpg';
+import colleenRowanZoom from './co/colleen-rowan-zoom.jpg';
+import craterLake from './co/crater-lake.jpg';
+import craterLakeZoom from './co/crater-lake-zoom.jpg';
+import parentsSelfie from './co/parents-selfie.jpg';
+import parentsSelfieZoom from './co/parents-selfie-zoom.jpg';
+import rocks from './co/rocks.jpg';
+import rocksZoom from './co/rocks-zoom.jpg';
+import stars from './co/stars.jpg';
+import starsZoom from './co/stars-zoom.jpg';
+
+// croton
+import bikeCamp from './croton/bike-camp.jpg';
+import bikeCampZoom from './croton/bike-camp-zoom.jpg';
+import campfire from './croton/campfire.jpg';
+import campfireZoom from './croton/campfire-zoom.jpg';
+import davidLamp from './croton/david-lamp.jpg';
+import davidLampZoom from './croton/david-lamp-zoom.jpg';
+import jamesLamp from './croton/james-lamp.jpg';
+import jamesLampZoom from './croton/james-lamp-zoom.jpg';
+
+// la
+import haCouple from './la/ha-couple.jpg';
+import haCoupleZoom from './la/ha-couple-zoom.jpg';
+import gulls from './la/gulls.jpg';
+import gullsZoom from './la/gulls-zoom.jpg';
+import hollyCam from './la/holly-cam.jpg';
+import hollyCamZoom from './la/holly-cam-zoom.jpg';
+import hollywood from './la/hollywood.jpg';
+import hollywoodZoom from './la/hollywood-zoom.jpg';
+import malibu from './la/malibu.jpg';
+import malibuZoom from './la/malibu-zoom.jpg';
+import roller from './la/roller.jpg';
+import rollerZoom from './la/roller-zoom.jpg';
+
+// nyc
+import bikeSelf from './nyc/bike-self.jpg';
+import bikeSelfZoom from './nyc/bike-self-zoom.jpg';
+import blueSun from './nyc/blue-sun.jpg';
+import blueSunZoom from './nyc/blue-sun-zoom.jpg';
+import candleSteps from './nyc/candle-steps.jpg';
+import candleStepsZoom from './nyc/candle-steps-zoom.jpg';
+import doorFlowers from './nyc/door-flowers.jpg';
+import doorFlowersZoom from './nyc/door-flowers-zoom.jpg';
+import duckie from './nyc/duckie.jpg';
+import duckieZoom from './nyc/duckie-zoom.jpg';
+import headlessMatt from './nyc/headless-matt.jpg';
+import headlessMattZoom from './nyc/headless-matt-zoom.jpg';
+import hollyInfinite from './nyc/holly-infinite.jpg';
+import hollyInfiniteZoom from './nyc/holly-infinite-zoom.jpg';
+import lamp from './nyc/lamp.jpg';
+import lampZoom from './nyc/lamp-zoom.jpg';
+import leafy from './nyc/leafy.jpg';
+import leafyZoom from './nyc/leafy-zoom.jpg';
+import manhattanBridge from './nyc/manhattan-bridge.jpg';
+import manhattanBridgeZoom from './nyc/manhattan-bridge-zoom.jpg';
+import trash from './nyc/trash.jpg';
+import trashZoom from './nyc/trash-zoom.jpg';
+
+// ny
+import hudson from './ny/hudson.jpg';
+import hudsonZoom from './ny/hudson-zoom.jpg';
+
+// sma
+import calleHuertas from './sma/calle-huertas.jpg';
+import calleHuertasZoom from './sma/calle-huertas-zoom.jpg';
+
+// tx
+import texasSunset from './tx/texas-sunset.jpg';
+import texasSunsetZoom from './tx/texas-sunset-zoom.jpg';
+
+// pa
+import fallLeaves from './pa/fall-leaves.jpg';
+import fallLeavesZoom from './pa/fall-leaves-zoom.jpg';
+
+// const proud = [
+//   { src: bikeSelf, zoomSrc: bikeSelfZoom },
+//   { src: doorFlowers, zoomSrc: doorFlowersZoom },
+//   { src: blueSun, zoomSrc: blueSunZoom, caption: 'FPO' }
+// ];
 
 const ny = [
+  { src: candleSteps, zoomSrc: candleStepsZoom },
+  { src: leafy, zoomSrc: leafyZoom },
+  { src: trash, zoomSrc: trashZoom },
   { src: hudson, zoomSrc: hudsonZoom, caption: 'The Hudson from the train' },
-  { src: candleSteps, zoomSrc: candleStepsZoom }
+  { src: lamp, zoomSrc: lampZoom },
+  { src: headlessMatt, zoomSrc: headlessMattZoom },
+  { src: manhattanBridge, zoomSrc: manhattanBridgeZoom }
 ];
 
 const croton = [
@@ -63,100 +119,109 @@ const croton = [
     src: davidLamp,
     zoomSrc: davidLampZoom,
     caption: 'David with ramen'
+  },
+  {
+    src: campfire,
+    zoomSrc: campfireZoom
   }
 ];
 
 const la = [
-  { src: hollywood, zoomSrc: hollywoodZoom },
+  { src: gulls, zoomSrc: gullsZoom },
+  { src: haCouple, zoomSrc: haCoupleZoom },
   {
     src: hollyCam,
-    zoomSrc: hollyCamZoom,
-    caption: 'Holly at Santa Monica pier'
-  }
-];
-
-// ny: lamp, hudson, candle, matt, manhattan-bridge, duck?, kusama
-// croton: bike, james, david, fire
-// tx: sunset
-// los angeles: seagulls, ha couple, roller, holly-cam, hollywood
-// arizona / colorado: canyon-riverview, canyon-sunrise, frybread, selfie, rocks, chanterelles,
-// philly?
-// san miguel?
-
-const spring = [
-  { src: hudson, zoomSrc: hudsonZoom, caption: 'The Hudson from the train' },
-  { src: candleSteps, zoomSrc: candleStepsZoom },
-  { src: texasSunset, zoomSrc: texasSunsetZoom }
-];
-
-const summer = [
-  {
-    src: bikeCamp,
-    zoomSrc: bikeCampZoom,
-    caption: 'Bike camping at Croton Point Park'
+    zoomSrc: hollyCamZoom
   },
-  { src: jamesLamp, zoomSrc: jamesLampZoom },
-  { src: davidLamp, zoomSrc: davidLampZoom },
+  { src: roller, zoomSrc: rollerZoom },
+  { src: malibu, zoomSrc: malibuZoom, caption: 'Malibu from Point Dume' },
+  { src: hollywood, zoomSrc: hollywoodZoom }
+];
+
+const az = [
   { src: canyonRiverview, zoomSrc: canyonRiverviewZoom },
+  { src: brittanCanyon, zoomSrc: brittanCanyonZoom },
   { src: canyonSunrise, zoomSrc: canyonSunriseZoom },
+  { src: frybread, zoomSrc: frybreadZoom }
+];
+
+const co = [
+  { src: beaverMountain, zoomSrc: beaverMountainZoom },
+  { src: stars, zoomSrc: starsZoom },
+  { src: craterLake, zoomSrc: craterLakeZoom },
   { src: parentsSelfie, zoomSrc: parentsSelfieZoom },
-  { src: hollywood, zoomSrc: hollywoodZoom },
+  { src: rocks, zoomSrc: rocksZoom },
+  { src: chanterelles, zoomSrc: chanterellesZoom },
+  { src: colleenRowan, zoomSrc: colleenRowanZoom }
+];
+
+const nyc = [
+  { src: duckie, zoomSrc: duckieZoom },
+  { src: hollyInfinite, zoomSrc: hollyInfiniteZoom },
+  { src: blueSun, zoomSrc: blueSunZoom }
+];
+
+const pa = [
   {
-    src: hollyCam,
-    zoomSrc: hollyCamZoom,
-    caption: 'Holly at Santa Monica pier'
+    src: fallLeaves,
+    zoomSrc: fallLeavesZoom,
+    caption: 'Pit stop near Hackettstown, NJ'
   }
 ];
 
-const fall = [{ src: blueSun, zoomSrc: blueSunZoom, caption: 'FPO' }];
+const sma = [
+  { src: calleHuertas, zoomSrc: calleHuertasZoom, caption: 'Calle Huertas' }
+];
 
-const winter = [{ src: blueSun, zoomSrc: blueSunZoom, caption: 'FPO' }];
+// const tx = [{ src: texasSunset, zoomSrc: texasSunsetZoom }];
 
 export {
-  proud,
+  // proud,
   ny,
   croton,
   la,
-  spring,
-  summer,
-  fall,
-  winter,
+  az,
+  co,
+  nyc,
+  sma,
+  pa,
+  // tx,
   bikeSelf,
   bikeSelfZoom,
   doorFlowers,
   doorFlowersZoom,
-  canyonRiverview,
-  canyonRiverviewZoom,
-  canyonSunrise,
-  canyonSunriseZoom,
-  leafy,
-  leafyZoom,
-  jamesLamp,
-  jamesLampZoom,
-  davidLamp,
-  davidLampZoom,
-  haCouple,
-  haCoupleZoom,
-  chanterelles,
-  chanterellesZoom,
-  candleSteps,
-  candleStepsZoom,
+  // canyonRiverview,
+  // canyonRiverviewZoom,
+  // canyonSunrise,
+  // canyonSunriseZoom,
+  // leafy,
+  // leafyZoom,
+  // jamesLamp,
+  // jamesLampZoom,
+  // davidLamp,
+  // davidLampZoom,
+  // haCouple,
+  // haCoupleZoom,
+  // chanterelles,
+  // chanterellesZoom,
+  // candleSteps,
+  // candleStepsZoom,
   blueSun,
   blueSunZoom,
-  hudson,
-  hudsonZoom,
-  bikeCamp,
-  bikeCampZoom,
-  frybread,
-  frybreadZoom,
-  hollywood,
-  hollywoodZoom,
+  // hudson,
+  // hudsonZoom,
+  // bikeCamp,
+  // bikeCampZoom,
+  // frybread,
+  // frybreadZoom,
+  // hollywood,
+  // hollywoodZoom,
   texasSunset,
-  texasSunsetZoom,
-  roller,
-  rollerZoom,
-  hollyCam,
-  hollyCamZoom,
-  parentsSelfie,
-  parentsSelfieZoom
+  texasSunsetZoom
+  // roller,
+  // rollerZoom,
+  // hollyCam,
+  // hollyCamZoom,
+  // parentsSelfie,
+  // parentsSelfieZoom
 };
